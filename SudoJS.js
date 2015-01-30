68a2c3e1eb30aa8e7332bc3e0a09616a882a0cc6
Jared Schwartz <jgs28@duke.edu>
2015-01-30 18:03:34 -0500

    Update SudoJS.js
    Added functionality to make table readonly on load, and editable on Start; fixed issue in error checking messages

--- SudoJS.js
+++ SudoJS.js
@@ -1,291 +1,199 @@
 /*
  * @author Jared Schwartz
- * @since 2014-10-01
+ * @since 2015-01-26
  */
 
 $(document).ready(function(){
 	$(window).load(function(){
+//		$("td>input").each(function(){
+//			$(this).prop('readonly', true);
+//		});
 		
-		// Pre-defined variables
-		var filterShown = []; // list of checked filters, used for undoing effects
-		var regions = $("#SanMarco, #Murano, #Castello"); // for hiding regions when showing filters
-		var buildings = $(".SanMarco, .Murano, .Castello");
-		var height = 580, width = 960; // viewbox dimensions
-		var curActiveID = ""; // current active building
-		
-		// Create defs and define clip-paths	
-		$("#Map").before("<defs> </defs>");
-		
-		/*
-		 * Generic function to create clip-paths of a path.
-		 * Input: origID of a path and stroke color
-		 * Purpose: Creates a clip-path, with id "origIDClip" using the bounding box of the path
-		 * and adds it to the defs element for later use
-		 */
-		var makeClipPath = function(elID, stroke){
-			var box = $(elID)[0].getBBox();
-			var clip = "<clipPath id=\"" + elID.replace("#","") + "Clip\"> <rect x="
-			+ box.x + " y="+box.y + " width="+box.width+" height="+box.height+" stroke=\""+stroke+"\"/> </clipPath>";
-			$("defs").append(clip);
-			return;
+		var setAlert = function(message){
+			$("#alert").text(message);
 		};
 		
-		// Make clip-path for each district for zooming
-		makeClipPath("#Duchal", "green");
-		makeClipPath("#Murano", "green");
-		makeClipPath("#ViewBox", "green");
-
-	
-		/*
-		 *  Generic function to calculate zoom params for any path using bounding box
-		 *  @param id of a region to zoom in on
-		 *  @return array with scalefactor and dimensions for zooming formula
-		 */
-		var zoomVariables = function(elID){
-			var box = $(elID)[0].getBBox();
-			// + 30, + 50 is to leave space around the path in the window
-			var scaleFactor = Math.min(height / (box.height+30), width / (box.width+50));
-			var zoomX = box.x + (box.width/2),
-				zoomY = box.y + (box.height/2);
-			return [scaleFactor, -zoomX, -zoomY, box.width, box.height];
-		};
+		var timer;
 		
-		
-		/*
-		 * Function to zoom in if the element is not zoomed, and zoom out if already zoomed
-		 * Adds transform attribute to wrapper element that has the zoom code
-		 * @param id of element
-		 */
-		var toggleZoom = function(elID){
-			if($("#wrapper").attr("transform")){
-				// already zoomed
-				$("#wrapper").removeAttr("transform");
-				$(".District").show();
-			} else {
-				var settings = zoomVariables(elID); // scaleFactor, zoomX, zoomY
-				$("#wrapper").attr("transform",
-						"translate(" + (-settings[1]/*-settings[3]*/) + "," + (-settings[2]/*+settings[4]*/) + ")" +
-						"scale(" + settings[0] + ")" +
-						"translate(" + settings[1] + "," + settings[2] + ")");
-				
-				// hide all other districts because switching between them while zoomed causes problems
-				$(".District:not("+elID+")").hide();
-			}
-			return;
-		};
-		
-//		$("wrapper").css("clip-path", "url(#ViewClip)");
-		// adds/removes clip-path 
-		
-		/*
-		 * Adds or removes a clip-path to the css of wrapper element
-		 * @param name of clip-path, typically "#RegionClip"
-		 */
-		var toggleClip = function(clipName){
-			var $wrap = $("#wrapper");
-			var wClip = $wrap.css("clip-path");
-			if(wClip != "url("+clipName+")" && clipName != ""){
-				$wrap.css("clip-path", "url("+clipName+")");
-			} else {
-				$wrap.css("clip-path", "");
-			};
-			return;
-		};
-		
-		
-		// onclick for Show/Hide Filters button; toggles the filter options
-		$("#btn-filter").click(function(){
-			if($(this).text() == "Show Filters"){
-				// show all filters
-				$(this).text("Hide Filters");
-				$("#Filters").show();
-			} else {
-				// hide filter options
-				$(this).text("Show Filters");
-				$("#Filters").hide();
-			}
-		});
-		
-		/*
-		 * Attached to submit button for filters
-		 * Removes all current filters, then uses list of checked filters to
-		 * add all that apply and highlight them on the map
-		 * hides regions so that they cannot be zoomed
-		 */
-		var onSubmit = function(){
-			removeActive(); // make sure that if already zoomed, no building still shows as active
-			for(var i=0; i<filterShown.length; i++){
-				// remove highlighted buildings from previous filters, if any
-				filterShown[i].classList.remove("highlight");
-			}
-			filterShown = []; // reset list of filters
-			var selected = [];
-			var districts = [];
-			var buildings = [];
-			$('#Filters input:checked').each(function() {
-			    selected.push($(this).attr('value')); // collect values of checked filters
-			    if($(this).attr('name') == "district"){
-			    	districts.push($(this).attr('value'));
-			    }
-			    if($(this).attr('name') == "building"){
-			    	buildings.push($(this).attr('value'));
-			    }
-			});
-			if(selected.length == 0){
-				return; // if no filters selected, return to end the function
-			}
-			$("#wrapper").removeAttr("transform"); // zoom out to full map
-			var showList = $([]); // initialize empty jQuery object so that $.merge works correctly
-			if(buildings.length > 0){
-				// build a list of all building elements selected by the building filters
-				$.each(buildings, function(index, value){
-					var list = $("."+value);
-					$.merge(showList, list); // adds all newly selected elements into showList
-				});
-				if(districts.length > 0){
-					// narrow building list down by only including buildings in checked regions
-					var districtSelector = "";
-					for(var i=0; i<districts.length; i++){
-						if(i == districts.length-1){
-							// dont add ", " to continue the string if it is the last district
-							districtSelector += "." + districts[i];
-							break;
-						}
-						// create list ".one, .two, .three" to make string of selectors to filter by
-						districtSelector += "." + districts[i] + ", ";
-					}
-					// filter list to only include checked districts
-					showList = showList.filter(districtSelector);
+		$("#start").click(function(){
+			timer = 
+			setInterval(function () {
+				var time = $("#timer").text();
+				var min = parseInt(time.split(":")[0]);
+				var sec = parseInt(time.split(":")[1]) + 1;
+				if(sec >= 60){
+					sec = 00;
+					min += 1;
 				}
-			} else{
-				// if building list is empty, just add every building in checked regions
-				$.each(districts, function(index, value){
-					var list = $("."+value);
-					showList.push.apply(showList, list);
-				});
-			}
+				if(sec < 10){
+					var secStr = "0" + sec.toString();
+					var minStr = min.toString();
+				} else {
+					var secStr = sec.toString();
+					var minStr = min.toString();
+				}
+				$("#timer").text(minStr + ":" + secStr);
+			}, 1000);
 			
-			// show selected buildings, add highlight class to make them appear,
-			// and keep array of highlighted elements so they are easy to remove
-			showList.show();
-			for(var i=0; i<showList.length; i++){	
-				showList[i].classList.add("highlight");
-				filterShown.push(showList[i]);
-			}
-			// hide regions so only the buildings show
-			for(var i=0; i<regions.length; i++){
-				$(regions[i]).hide();
-			}
-		};
-		
-		// onclick for submit button to run onSubmit()
-		$("#Submit").click(function(){
-			onSubmit();
-		});
-		
-		/*
-		 * Attached to Clear Map button in filters
-		 * Removes all highlighted buildings and shows regions for zooming
-		 */
-		$("#btn-clear-map").click(function(){
-			for(var i=0; i<filterShown.length; i++){
-				// clear highlighted buildings
-				filterShown[i].classList.remove("highlight");
-			}
-			buildings.hide();
-
-			// show regions again so map is reset to original settings
-			regions.show();
+			$("#pause").prop("disabled", false);
 			
-			removeActive();
-			$(".info").hide();
-			filterShown = []; // reset used filters list
+			$(".guess").each(function(){
+				$(this).prop('readonly', false);
+			});
 		});
 		
+		$("#pause").click(function() {
+			clearInterval(timer);
+			$(".guess").each(function(){
+				$(this).prop('readonly', true);
+			});
+		});
+		
+		var sc1 = '[name="'; // sc = sudo call; easy to insert when making a lot of jQuery calls for sudo-01, etc
+		var sc2 =	'"]';
+		
+		var row0 = ["sudo-01","sudo-02","sudo-03","sudo-04","sudo-05","sudo-06","sudo-07","sudo-08","sudo-09"];
+		var row1 = ["sudo-11","sudo-12","sudo-13","sudo-14","sudo-15","sudo-16","sudo-17","sudo-18","sudo-19"];
+		var row2 = ["sudo-21","sudo-22","sudo-23","sudo-24","sudo-25","sudo-26","sudo-27","sudo-28","sudo-29"];
+		var row3 = ["sudo-31","sudo-32","sudo-33","sudo-34","sudo-35","sudo-36","sudo-37","sudo-38","sudo-39"];
+		var row4 = ["sudo-41","sudo-42","sudo-43","sudo-44","sudo-45","sudo-46","sudo-47","sudo-48","sudo-49"];
+		var row5 = ["sudo-51","sudo-52","sudo-53","sudo-54","sudo-55","sudo-56","sudo-57","sudo-58","sudo-59"];
+		var row6 = ["sudo-61","sudo-62","sudo-63","sudo-64","sudo-65","sudo-66","sudo-67","sudo-68","sudo-69"];
+		var row7 = ["sudo-71","sudo-72","sudo-73","sudo-74","sudo-75","sudo-76","sudo-77","sudo-78","sudo-79"];
+		var row8 = ["sudo-81","sudo-82","sudo-83","sudo-84","sudo-85","sudo-86","sudo-87","sudo-88","sudo-89"];
+		var rowCollect = [row0,row1,row2,row3,row4,row5,row6,row7,row8];
+		
+		var col0 = ["sudo-01","sudo-11","sudo21","sudo-31","sudo-41","sudo-51","sudo-61","sudo-71","sudo-81"];
+		var col1 = ["sudo-02","sudo-12","sudo22","sudo-32","sudo-42","sudo-52","sudo-62","sudo-72","sudo-82"];
+		var col2 = ["sudo-03","sudo-13","sudo23","sudo-33","sudo-43","sudo-53","sudo-63","sudo-73","sudo-83"];
+		var col3 = ["sudo-04","sudo-14","sudo24","sudo-34","sudo-44","sudo-54","sudo-64","sudo-74","sudo-84"];
+		var col4 = ["sudo-05","sudo-15","sudo25","sudo-35","sudo-45","sudo-55","sudo-65","sudo-75","sudo-85"];
+		var col5 = ["sudo-06","sudo-16","sudo26","sudo-36","sudo-46","sudo-56","sudo-66","sudo-76","sudo-86"];
+		var col6 = ["sudo-07","sudo-17","sudo27","sudo-37","sudo-47","sudo-57","sudo-67","sudo-77","sudo-87"];
+		var col7 = ["sudo-08","sudo-18","sudo28","sudo-38","sudo-48","sudo-58","sudo-68","sudo-78","sudo-88"];
+		var col8 = ["sudo-09","sudo-19","sudo29","sudo-39","sudo-49","sudo-59","sudo-69","sudo-79","sudo-89"];
+		var colCollect = [col0,col1,col2,col3,col4,col5,col6,col7,col8];
+		
+		var grid0 = ["sudo-01","sudo-02","sudo-03","sudo-11","sudo-12","sudo-13","sudo-21","sudo-22","sudo-23"];
+		var grid1 = ["sudo-04","sudo-05","sudo-06","sudo-14","sudo-15","sudo-16","sudo-24","sudo-25","sudo-26"];
+		var grid2 = ["sudo-07","sudo-08","sudo-09","sudo-17","sudo-18","sudo-19","sudo-27","sudo-28","sudo-29"];
+		var grid3 = ["sudo-31","sudo-32","sudo-33","sudo-41","sudo-42","sudo-43","sudo-51","sudo-52","sudo-53"];
+		var grid4 = ["sudo-34","sudo-35","sudo-36","sudo-44","sudo-45","sudo-46","sudo-54","sudo-55","sudo-56"];
+		var grid5 = ["sudo-37","sudo-38","sudo-39","sudo-47","sudo-48","sudo-49","sudo-57","sudo-58","sudo-59"];
+		var grid6 = ["sudo-61","sudo-62","sudo-63","sudo-71","sudo-72","sudo-73","sudo-81","sudo-82","sudo-83"];
+		var grid7 = ["sudo-64","sudo-65","sudo-66","sudo-74","sudo-75","sudo-76","sudo-84","sudo-85","sudo-86"];
+		var grid8 = ["sudo-67","sudo-68","sudo-69","sudo-77","sudo-78","sudo-79","sudo-87","sudo-88","sudo-89"];
+		var gridCollect = [grid0,grid1,grid2,grid3,grid4,grid5,grid6,grid7,grid8];
+		
+		var validateBoard = function(subBool){
+			for(var j=0; j<9; j++){
+				var curRow = rowCollect[j];
+				var curCol = colCollect[j];
+				var curGrid = gridCollect[j];
+				var valRows = [0,0,0,0,0,0,0,0,0,0];
+				var valCols = [0,0,0,0,0,0,0,0,0,0];
+				var valGrids = [0,0,0,0,0,0,0,0,0,0];
+				for(var i=0; i<9; i++){
+					var rowInd = " ABCDEFGHI".indexOf($(sc1 + curRow[i] + sc2).val());
+					if(subBool && rowInd === 0){
+						setAlert("Board not yet completed.");
+						return false; // checks for empty squares if board is being submitted, not validated
+					}
+					if(rowInd > 0){  // empty space defaults to index 0 for some reason
+						if(valRows[rowInd] !== 0){
+							setAlert("Check failed on Row " + (j+1).toString());
+							return false;
+						} else {
+							valRows[rowInd] = 1;
+						}
+					}
 
-		// Section of onclick methods for each region
-		// On zoom in: zooms, changed fill color, shows buildings
-		// On zoom out: reverts fill color, hides building info if shown, hides buildings
-		$("#SanMarco").click( function(){
-//			toggleClip("#DuchalClip");
-			toggleZoom("#SanMarco"); // zooms if not zoomed and vice versa
-			if($("#wrapper").attr("transform")){
-				// if togglezoom just zoomed (added transform attr), show buildings in the region and grey the region
-				$("#SanMarco").css("fill","#000000");
-				$(".SanMarco").show();
-			} else {
-				// if just zoomed out, revert region color and hide buildings & info
-				$("#SanMarco").css("fill","#ace63c");
-				$(".info").hide();
-				$(".SanMarco").hide();
-				removeActive();
+					var colInd = " ABCDEFGHI".indexOf($(sc1 + curCol[i] + sc2).val());
+					if(subBool && colInd === 0){
+						setAlert("Board not yet completed.");
+						return false;
+					}
+					if(colInd > 0){
+						if(valCols[colInd] !== 0){
+							setAlert("Check failed on Column " + (j+1).toString());
+							return false;
+						} else {
+							valCols[colInd] = 1;
+						}
+					}
+
+					var gridInd = " ABCDEFGHI".indexOf($(sc1 + curGrid[i] + sc2).val());
+					if(subBool && gridInd === 0){
+						setAlert("Board not yet completed.");
+						return false;
+					}
+					if(gridInd > 0){
+						if(valGrids[gridInd] !== 0){
+							setAlert("Check failed on Grid " + (j+1).toString());
+							return false;
+						} else {
+							valGrids[gridInd] = 1;
+						}
+					}
+				}
 			}
-//			$(".SanMarco").toggle();
-			} );
-		$("#Castello").click( function(){
-//			toggleClip("#DuchalClip");
-			toggleZoom("#Castello");
-			if($("#wrapper").attr("transform")){
-				$("#Castello").css("fill","#000000");
-				$(".Castello").show();
-			} else {
-				$("#Castello").css("fill","#ace63c");
-				$(".info").hide();
-				$(".Castello").hide();
-				removeActive();
-			}
-			} );
-		$("#Murano").click( function(){
-			// special case because the building and region are the same shape
-			// treat region as the building on zoom, and only use building for filter/highlight
-			toggleZoom("#Murano");
-			if($("#wrapper").attr("transform")){
-				$("#Murano").css("fill","#000000");
-				$("#MuranoInfo").show();
-				toggleActiveClass("#Murano");
-			} else {
-				$("#Murano").css("fill","#ace63c");
-				$(".info").hide();
-				$(".Murano").hide();
-				removeActive();
-			}
-			$("#wrapper").addClass("clipped");
-			} );
-		
-		// switch the active building from current to the one in elID
-		var toggleActiveClass = function(elID){
-			if(curActiveID.length > 0){
-				$(curActiveID)[0].classList.remove("active");
-			}
-			$(elID)[0].classList.add("active");
-			curActiveID = elID;
+			return true;
 		};
 		
-		// Remove active class from whatever building is active; used when filters are shown
-		var removeActive = function(){
-			if(curActiveID.length > 0){
-				$(curActiveID)[0].classList.remove("active");
+		var verifySingleInput = function(name){
+			var call = '[name="'+name+'"]';
+			var square = $(call).val();
+			if(square !== undefined){
+				if(square.length > 0){
+					square = square.toUpperCase();
+				}
+				if(square.length > 1 || " ABCDEFGHI".indexOf(square) === -1){
+					setAlert("Boxes must be filled with a valid drive letter: ABCDEFGHI");
+					$(call).val("");
+					return;
+				}
+				setAlert("");
+				$(call).val(square);
 			}
-			curActiveID = "";
+			return;
 		};
 		
-		// Section of onclick methods for buildings
-		// shows building as active and display info
-		// every line expanded looks like the Duchal function below
-		$("#Duchal").click(function(){
-			$(".info").hide();
-			$("#DuchalInfo").toggle();
-			toggleActiveClass("#Duchal");
+		$("input").blur(function(){
+			verifySingleInput(this.name);
 		});
-		$("#Build1").click(function(){$(".info").hide();$("#Build1Info").toggle();toggleActiveClass("#Build1");});
-		$("#Build2").click(function(){$(".info").hide();$("#Build2Info").toggle();toggleActiveClass("#Build2");});
-		$("#Build3").click(function(){$(".info").hide();$("#Build3Info").toggle();toggleActiveClass("#Build3");});
-		$("#Build4").click(function(){$(".info").hide();$("#Build4Info").toggle();toggleActiveClass("#Build4");});
-		$("#MuranoBuild").click(function(){
-			$(".info").hide();$("#MuranoInfo").toggle();toggleActiveClass("#MuranoBuild");
-			// special case because the building and region are the same shape
-			if($("#wrapper").attr("transform")){
-				toggleZoom(".Murano");
-			}});
+		
+		$("#validate").click(function(){
+			$("#alert").text($("#alert").text() + "\nValidating...");
+			var t = 0;
+			var timer = setInterval(function(){t+=1;},1000);
+			if(validateBoard(false)){ // alert set within function if it fails
+				setAlert("Check passed okay.");
+			}
+//			alert("Validation took "+t.toString()+" seconds.");
+		});
+		
+		$("#submit").click(function(){
+			if(validateBoard(true)){
+				var t = $("#timer").text();
+				var m = parseInt(t.split(":")[0]);
+				var s = parseInt(t.split(":")[1]);
+				var mStr = "";
+				var sStr = s.toString() + " seconds!";
+				if(s === 1){
+					sStr = s.toString() + " second!";
+				}
+				if(m > 0){
+					if(m === 1){
+						mStr = m.toString() + " minute and ";
+					} else {
+						mStr = m.toString() + " minutes and ";
+					}
+				}
+		
+				setAlert("Congratulations! You solved the board in " + mStr + sStr);
+				$(":button").prop("disabled", true);
+				clearInterval(timer);
+			}
+		});
 	});
 });
